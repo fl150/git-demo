6c8e32b01839cb47108d7dc609589bbd3d3cad49
fl150 <503832344@qq.com>
2020-10-26 14:58:12 +0800

    v0.7

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+	@GetMapping("/")
+	public String hello() {
+		return "hello git - v0.1";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
